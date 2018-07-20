50defd0ef3bda16ad5996aec968e3ef47e0fbf5b
Ed Perkins <ed@edperkins.co.uk>
2018-07-20 16:13:13 +0100

    add meal

---
+++ src/meal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Meal = props => (
+  <ul>
+    <li>smoked salmon</li>
+    <li>asparagus</li>
+    <li>courgette</li>
+    <li>single cream</li>
+    <li>dill</li>
+    <li>lemon</li>
+    <li>linguine</li>
+  </ul>
+)
+
+export default Meal;
