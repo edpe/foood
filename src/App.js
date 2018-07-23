2c8e04d0adb6b26c784dcf4b16bd9f3936f6882c
Ed Perkins <ed@edperkins.co.uk>
2018-07-23 12:05:35 +0100

    store menu selection in state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Meal from './meal';
+import Menu from './menu';
 
-const App = props => (
-  <h2>foood</h2>
 
-)
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {selectedMeal: ""}
+
+  }
+
+  handleChange = (evt) => {
+    this.setState({selectedMeal: evt.target.value})
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>foood</h2>
+        {this.state.selectedMeal}
+        <hr/>
+        <Menu onChange={this.handleChange} />
+      </div>
+    )
+  }
+}
 
 export default App;
