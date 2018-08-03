8452d557f7b751a45238677e61ab45911e2eac1d
Ed Perkins <ed@edperkins.co.uk>
2018-08-03 13:37:54 +0100

    add some styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,52 @@ import React, { Component } from 'react';
 import Menu from './menu';
 import data from './data.json';
 
+
+const main = {
+  display: "flex",
+  flex: 1,
+  fontFamily: "Open Sans Condensed",
+}
+
+const ingredients = {
+  textAlign: "left",
+  listStyleType: "none",
+  marginRight: "20px",
+  fontSize: "1.5rem"
+}
+
+const foodImage = {
+  width: "100%",
+  height: "auto",
+  paddingRight: "5%"
+}
+
+const title = {
+  fontFamily: "garamond",
+  fontSize: "4rem",
+  marginTop: "0.1em",
+  marginRight: "5%",
+  marginBottom: "0.1em",
+  textAlign: "right",
+}
+
+const article = {
+  flex: "50%"
+}
+
+const aside = {
+  flex: "50%"
+}
+
+const styles = {
+  foodImage: foodImage,
+  main: main,
+  title: title,
+  ingredients: ingredients,
+  article: article,
+  aside: aside
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -33,14 +79,15 @@ class App extends Component {
       <div>
         <h2 style={styles.title}>foood</h2>
         <hr/>
-        <Menu style={styles.menu} onChange={this.handleChange} />
+        <Menu style onChange={this.handleChange}/>
+        <hr/>
         <main style={styles.main}>
-          <article>
-            <ul>{listIngredients}</ul>
-            <ol>{listMethod}</ol>
+          <article style={styles.article}>
+            <ul style={styles.ingredients}>{listIngredients}</ul>
+            <ol style={styles.ingredients}>{listMethod}</ol>
           </article>
-          <aside>
-            <img style={styles.foodImage} src={selectedRecipe.image}/>
+          <aside style={styles.aside}>
+            <img style={styles.foodImage} src={selectedRecipe.image} alt={this.state.selectedMeal}/>
           </aside>
         </main>
       </div>
@@ -48,32 +95,5 @@ class App extends Component {
   }
 }
 
-const main = {
-  display: "flex",
-  flex: 1,
-  fontFamily: "garamond"
-}
-
-const foodImage = {
-  width: "500px",
-  height: "250px",
-  marginLeft: 20
-}
-
-const title = {
-  fontFamily: "garamond",
-  fontSize: "4rem"
-}
-
-const menu = {
-  fontFamily: "garamond"
-}
-
-const styles = {
-  foodImage: foodImage,
-  main: main,
-  title: title,
-  menu: menu
-}
 
 export default App;
--- src/menu.js
+++ src/menu.js
@@ -2,13 +2,29 @@ import React from 'react';
 
 const Menu = props => (
   <div>
-    <label htmlFor="recipe-select">Choose a recipe:</label>
-    <select id="recipe-select" onChange={props.onChange}>
-        <option value="">--Please choose an option--</option>
+    <select style={styles.chooseRecipe}id="recipe-select" onChange={props.onChange}>
         <option value="salmonLinguine">Salmon Linguine</option>
         <option value="greenChickenCurry">Green Thai Chicken Curry</option>
     </select>
   </div>
 )
+
+const chooseRecipe = {
+  fontFamily: "Open Sans Condensed",
+  fontSize: "2rem",
+  fontWeight: 500,
+  marginBottom: "10px",
+  marginLeft: "35%",
+  textAlignLast: "center",
+  border: "0px",
+  outline: "0px",
+  marginTop: "2%",
+  backgroundColour: "white"
+
+}
+
+const styles = {
+  chooseRecipe: chooseRecipe
+}
 
 export default Menu;
