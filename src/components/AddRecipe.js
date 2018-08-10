ecf09ba198b04162ac00407623bf6eabe4b14190
Ed Perkins <ed@edperkins.co.uk>
2018-08-10 17:47:12 +0100

    add styles to AddRecipe

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import styles from './AddRecipe.module.css'
 
-const AddRecipe = props => (
-  <div>
-    <h2>add a recipe</h2>
-  </div>
-);
+
+const AddRecipe = props => {
+  return (
+      <div>
+        <h2>add a recipe</h2>
+      </div>
+  );
+};
 
 export default AddRecipe;
