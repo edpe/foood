7d1a733019b6a4ab265e54b494204430ba77d743
Ed Perkins <ed@edperkins.co.uk>
2018-08-13 15:06:42 +0100

    add from field to AddRecipe

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './AddRecipe.module.css'
 
 
-const AddRecipe = props => {
-  return (
-      <div>
-        <h2>add a recipe</h2>
+class AddRecipe extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {value: ''};
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+      alert(this.state.value);
+      event.preventDefault();
+    }
+
+    render() {
+      return (
+        <div className="container">
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Add Recipe:
+              <input type="text" value={this.state.value} onChange={this.handleChange} className="form-control"/>
+            </label><br/>
+            <input type="submit" value="Submit" className="btn btn-primary"/>
+          </form>
       </div>
-  );
-};
+      );
+    }
+  }
 
 export default AddRecipe;
