4d5ae378beab93a111dd9cd4e394aec30ab07dce
Ed Perkins <ed@edperkins.co.uk>
2018-08-14 13:37:38 +0100

    add update method to ItemService

--- src/components/ItemService.js
+++ src/components/ItemService.js
@@ -4,14 +4,23 @@ class ItemService {
 
   sendData(data) {
     axios.post('http://localhost:4200/items/add/post', {
-    item: data
-  })
-  .then(function (response) {
-      console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+      item: data
+    })
+    .then(res => this.setState({ items: res.data }))
+    .catch(err => console.log(err))
+  }
+
+  updateData(data, id){
+    axios.post('http://localhost:4200/items/update/'+id, {
+      item: data
+    })
+    .then(res => this.setState({ items: res.data }))
+    .catch(err => console.log(err))
+  }
+
+  deleteData(id){
+    axios.get('http://localhost:4200/items/delete/'+id)
+    .then().catch(err => console.log(err))
   }
 }
 
