95aec6a189da2f6d08fd02ef41bc010a5f2fd4a9
Ed Perkins <ed@edperkins.co.uk>
2018-08-14 11:22:21 +0100

    add TableRow

---
+++ src/components/TableRow.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class TableRow extends Component {
+  render() {
+    return (
+        <tr>
+          <td>
+            {this.props.obj._id}
+          </td>
+          <td>
+            {this.props.obj.item}
+          </td>
+          <td>
+            <button className="btn btn-primary">Edit</button>
+          </td>
+          <td>
+            <button className="btn btn-danger">Delete</button>
+          </td>
+        </tr>
+    );
+  }
+}
+
+export default TableRow;
