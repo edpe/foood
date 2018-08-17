5410bc25644bb6a0ed0a5b3cbcb3b1791461f6e5
edpe <ed@edperkins.co.uk>
2018-08-17 15:35:01 +0100

    export recipe object to back end

--- src/components/RecipeService.js
+++ src/components/RecipeService.js
@@ -1,26 +1,37 @@
-import axios from 'axios';
+import axios from "axios";
 
 class RecipeService {
-
   sendData(data) {
-    axios.post('http://localhost:4200/recipes/add/post', {
-      recipe: data
-    })
-    .then(res => this.setState({ recipes: res.data }))
-    .catch(err => console.log(err))
+    axios
+      .post("http://localhost:4200/recipes/add/post", data)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   }
 
-  updateData(data, id){
-    axios.post('http://localhost:4200/recipes/update/'+id, {
-      recipe: data
-    })
-    .then(res => this.setState({ recipes: res.data }))
-    .catch(err => console.log(err))
+  updateData(data, id) {
+    axios
+      .post("http://localhost:4200/recipes/update/" + id, {
+        recipe: {
+          title: data.title,
+          duration: data.duration
+        }
+      })
+      .then(res =>
+        this.setState({
+          recipe: {
+            title: data.title,
+            duration: data.duration
+          }
+        })
+      )
+      .catch(err => console.log(err));
   }
 
-  deleteData(id){
-    axios.get('http://localhost:4200/recipes/delete/'+id)
-    .then().catch(err => console.log(err))
+  deleteData(id) {
+    axios
+      .get("http://localhost:4200/recipes/delete/" + id)
+      .then()
+      .catch(err => console.log(err));
   }
 }
 
