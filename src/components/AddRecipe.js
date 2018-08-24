d55d729aac00761a64c0df0542c1cb412256eeba
Ed Perkins <ed@edperkins.co.uk>
2018-08-24 15:32:40 +0100

    39 full recipe form (#40)
    * feat: add image and text field to form and state
    
    * feat: storing ingredients objects in state
    
    * feat: add ingredient buttons
    
    * feat: remove ingredient button
    
    * feat: add and remove ingredients from state
    
    * fix: syntax issues

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import IngredientForm from "./IngredientForm";
 import RecipeService from "./RecipeService";
 
 class AddRecipe extends Component {
@@ -7,7 +8,9 @@ class AddRecipe extends Component {
     this.state = {
       recipe: {
         title: "",
-        duration: ""
+        image: "",
+        duration: "",
+        ingredients: [{ title: "", quantity: "", measurement: "" }]
       }
     };
     this.addRecipeService = new RecipeService();
@@ -17,16 +20,40 @@ class AddRecipe extends Component {
   }
 
   handleChange(event) {
-    const target = event.target;
+    const target = event.currentTarget;
     const value = target.value;
     const name = target.name;
-
     this.setState(state => ({
       recipe: {
         ...state.recipe,
         [name]: value
       }
     }));
+  }
+
+  onIngredientFormChange(evt, targetIndex) {
+    const currentTarget = evt.currentTarget;
+
+    this.setState(state => {
+      const ingredients = state.recipe.ingredients;
+      const ingredientField = currentTarget.name;
+      const ingredientValue = currentTarget.value;
+      return {
+        recipe: {
+          ...state.recipe,
+          ingredients: ingredients.map((oldIngredientForm, i) => {
+            if (i === targetIndex) {
+              return {
+                ...oldIngredientForm,
+                [ingredientField]: ingredientValue
+              };
+            } else {
+              return oldIngredientForm;
+            }
+          })
+        }
+      };
+    });
   }
 
   handleSubmit(event) {
@@ -40,7 +67,7 @@ class AddRecipe extends Component {
       <div className="container">
         <form onSubmit={this.handleSubmit}>
           <label>
-            Recipe Title:
+            Title:
             <input
               name="title"
               type="text"
@@ -56,6 +83,58 @@ class AddRecipe extends Component {
               name="duration"
               type="text"
               value={this.state.recipe.duration}
+              onChange={this.handleChange}
+              className="form-control"
+            />
+          </label>
+          <br />
+
+          {this.state.recipe.ingredients.map((ing, i) => (
+            <IngredientForm
+              key={i}
+              onChange={evt => this.onIngredientFormChange(evt, i)}
+              name={ing.title}
+              quantity={ing.quantity}
+              measurement={ing.measurement}
+            />
+          ))}
+
+          <button
+            type="button"
+            onClick={() =>
+              this.setState(oldState => ({
+                recipe: {
+                  ...oldState.recipe,
+                  ingredients: oldState.recipe.ingredients.concat({
+                    title: "",
+                    measurement: "",
+                    quantity: ""
+                  })
+                }
+              }))
+            }
+            children="Add Ingredient"
+          />
+
+          <button
+            type="button"
+            onClick={() =>
+              this.setState(oldState => ({
+                recipe: {
+                  ...oldState.recipe,
+                  ingredients: oldState.recipe.ingredients.slice(0, -1)
+                }
+              }))
+            }
+            children="Remove Ingredient"
+          />
+
+          <label>
+            Image:
+            <input
+              name="image"
+              type="text"
+              value={this.state.image}
               onChange={this.handleChange}
               className="form-control"
             />
